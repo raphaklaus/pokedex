36454044fdddba07f8357ec3f39c6029008703da
Raphael Klaus <raphaklaus@gmail.com>
2019-08-27 22:01:27 +0200

    chore: new structure

--- client/specs/main.spec.js
+++
@@ -1,8 +0,0 @@
-const chai = require('chai'),
-  expect = chai.expect,
-  chaiAsPromised = require('chai-as-promised');
-
-chai.use(chaiAsPromised);
-
-require('./home/home.spec')(expect);
-require('./list/list.spec')(expect);
